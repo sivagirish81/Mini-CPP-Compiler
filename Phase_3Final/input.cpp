02e2e17bf573b7961852ae6145148132a1547a70
R Siva Girish <sivagirish81@pop-os.localdomain>
2020-03-10 18:37:13 +0530

    After a lot of pain AST is done

---
+++ Phase_3Final/input.cpp
@@ -0,0 +1,32 @@
+int main()
+{
+
+    int c = 2;
+    int f = 4;
+    int d = 4;
+    int e = 5;
+    
+    int b = 4;
+    int a = 3;
+    if(a < b)
+    {
+        int a = 4;
+        int b = 6;
+        a = b;
+	}
+    else if(1+3)
+    {
+        2 + 3;
+    }
+    else
+    {
+        int a = 4;
+        int c = a + 4;
+
+    }
+	while (c)
+	{
+		a = b + c;
+	}
+	
+}
