6c6c137de3eb5c78f8b9c789ab221c021b556e4a
mayankagarwal44442 <mayankagarwal44442@gmail.com>
2020-04-07 16:01:58 +0530

    added more inputs

---
+++ Phase_4/Inputs/Mixed/mixed.cpp
@@ -0,0 +1,16 @@
+int main()
+{
+    int a = 4;
+    for(int i = 1; i < 10; i += 1)
+    {
+        int b = 6;
+        while(b < 6)
+        {
+            int c = 4;
+            if( c < 5)
+            {
+                e = 7;
+            }
+        }
+    }
+}
