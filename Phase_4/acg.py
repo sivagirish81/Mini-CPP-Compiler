3efcb2f91448a8b955c5bec2a36c94a907490cf2
Siva Girish <sivagirish81@gmail.com>
2020-05-21 22:43:21 +0530

    Assembly Code Register Reallocation Done

--- Phase_4/acg.py
+++ Phase_4/acg.py
@@ -14,11 +14,14 @@ def read_quadruples(file):
 def remove_ifs_and_nots(instructions):
 	to_be_removed = list()
 	for i in range(len(instructions)):
-		if instructions[i][0] == "not":
-			instructions[i-1][3] = instructions[i+1][3]
-			to_be_removed.append(i)
-		elif instructions[i][0] == "if":
-			to_be_removed.append(i)
+		try:
+			if instructions[i][0] == "not":
+				instructions[i-1][3] = instructions[i+1][3]
+				to_be_removed.append(i)
+			elif instructions[i][0] == "if":
+				to_be_removed.append(i)
+		except:
+			continue
 
 	to_be_removed.sort(reverse = True)	
 	for index in to_be_removed:		#deleting from behind so that index doesnt change
@@ -43,11 +46,12 @@ def LRU_Free_Register(value):
 	common_variable_lines_mapping.pop(value)
 	
 	
-def getRegister(value, to_be_loaded):	# remember to load when returned. Also rtv code and 0 index
+def reg_alloc(value, to_be_loaded):	# remember to load when returned. Also rtv code and 0 index
 	global available_registers
 	global variable_register_mapping
 	global used_registers
 	if(len(available_registers) == 0):
+		# print(used_registers)
 		reg = used_registers.pop(0)
 		l = list(variable_register_mapping.keys())
 		for i in l:
@@ -67,7 +71,8 @@ def getRegister(value, to_be_loaded):	# remember to load when returned. Also rtv
 				print("LD", "R" + str(available_registers[0]), value)
 			to_return = "R" + str(available_registers[0])
 			available_registers.pop(0)
-			used_registers.append(variable_register_mapping[value])
+			if (variable_register_mapping[value] not in used_registers):
+				used_registers.append(variable_register_mapping[value])
 			return to_return
 
 def add_variable_to_line_mapping(variable, line, mapping):
@@ -81,8 +86,9 @@ def map_variable_lines(instructions):
 
 	global source_variable_lines_mapping
 	global common_variable_lines_mapping
-
+	# print(len(instructions))
 	for i in range(len(instructions)):
+		# print(i)
 		if instructions[i][0] in ["Label", "goto"]:			# label and goto have no use of registers
 			pass
 		elif instructions[i][0] in ["<", "<=", ">", ">=", "==", "!="]: 
@@ -110,7 +116,7 @@ def map_variable_lines(instructions):
 
 	
 
-def release_registers():
+def reg_dealloc():
 
 	global variable_register_mapping
 	global available_registers
@@ -120,25 +126,26 @@ def release_registers():
 	variables_used = [i for i in variable_register_mapping if not i.isnumeric()]
 	for i in variables_used:
 		register = variable_register_mapping[i]
-		if(len(common_variable_lines_mapping[i]) >= len(source_variable_lines_mapping[i])):
-			print("ST ", i, "R" + str(register))
+		if(i in common_variable_lines_mapping and i in source_variable_lines_mapping):
+			if(len(common_variable_lines_mapping[i]) >= len(source_variable_lines_mapping[i])):
+				print("ST ", i, "R" + str(register))
 
 	variable_register_mapping = {}
 	available_registers = initialize_register_list()
 
 def check_source(value, line):
 	
-	
-	source_variable_lines_mapping[value].remove(line)
-	common_variable_lines_mapping[value].remove(line)
-	if(len(common_variable_lines_mapping[value]) == 0 and not value.isnumeric()):
-		reg = variable_register_mapping[value]
-		if(value[0] != 'T'):
-			if(len(common_variable_lines_mapping[value]) >= len(source_variable_lines_mapping[value])):
-				print("ST", value, "R" + str(reg))
-		variable_register_mapping.pop(value)
-		available_registers.insert(0, reg)
-		available_registers.sort()
+	if(value in common_variable_lines_mapping and value in source_variable_lines_mapping):
+		source_variable_lines_mapping[value].remove(line)
+		common_variable_lines_mapping[value].remove(line)
+		if(len(common_variable_lines_mapping[value]) == 0 and not value.isnumeric()):
+			reg = variable_register_mapping[value]
+			if(value[0] != 'T'):
+				if(len(common_variable_lines_mapping[value]) >= len(source_variable_lines_mapping[value])):
+					print("ST", value, "R" + str(reg))
+			variable_register_mapping.pop(value)
+			available_registers.insert(0, reg)
+			available_registers.sort()
 
 
 def check_destination(value, line):
@@ -169,14 +176,14 @@ def assembly_gen(instructions):
 	for i in range(len(instructions)):
 		
 		if(instructions[i][0] == "Label"):
-			release_registers()		
+			reg_dealloc()		
 			print(instructions[i][3] + ":")
 		elif(instructions[i][0] == "goto"):
-			release_registers()
+			reg_dealloc()
 			print("BR " + instructions[i][3])
 		elif instructions[i][0] in ["<", "<=", ">", ">=", "==", "!="]: 
-			reg1 = getRegister(instructions[i][1], 1)
-			reg2 = getRegister(instructions[i][2], 1)
+			reg1 = reg_alloc(instructions[i][1], 1)
+			reg2 = reg_alloc(instructions[i][2], 1)
 			print("CMP", reg1, reg2)
 			if instructions[i][0] == "<":
 				print("BLT", instructions[i][3])
@@ -197,9 +204,9 @@ def assembly_gen(instructions):
 			common_variable_lines_mapping[instructions[i][2]].remove(i)
 
 		elif instructions[i][0] in ["+", "-", "*", "/", "&&", "||"]: 
-			reg1 = getRegister(instructions[i][1], 1)
-			reg2 = getRegister(instructions[i][2], 1)		
-			reg3 = getRegister(instructions[i][3], 0)
+			reg1 = reg_alloc(instructions[i][1], 1)
+			reg2 = reg_alloc(instructions[i][2], 1)		
+			reg3 = reg_alloc(instructions[i][3], 0)
 			
 
 			if instructions[i][0] == "+":
@@ -221,16 +228,16 @@ def assembly_gen(instructions):
 		
 
 		else:
-			reg1 = getRegister(instructions[i][3], 0)		
-			reg2 = getRegister(instructions[i][1], 1)
+			reg1 = reg_alloc(instructions[i][3], 0)		
+			reg2 = reg_alloc(instructions[i][1], 1)
 			print("MOV", reg1, reg2)
 
 			check_source(instructions[i][1], i)
 			check_destination(instructions[i][3], i)		
 
-instructions = read_quadruples("threeAddressCode.txt")
+instructions = read_quadruples("mix.txt")
 
-number_of_registers = 16
+number_of_registers = 2
 available_registers = initialize_register_list()
 variable_register_mapping = {}	
 # Which register is variable stored in. Useful to avoid redundant registers
